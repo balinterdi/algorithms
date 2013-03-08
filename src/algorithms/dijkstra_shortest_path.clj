39dfda9f7bc5fe5a45381b0e977263be6c8a26e6
Balint Erdi <balint.erdi@gmail.com>
2013-03-08 14:02:11 +0100

    Dijkstra's shortest path

---
+++ src/algorithms/dijkstra_shortest_path.clj
@@ -0,0 +1,62 @@
+(ns algorithms.dijkstra-shortest-path
+  (:use  [algorithms.core :only [load-weighted-graph]] ))
+
+(def infinity 1000000) ; could be Integer/MAX_VALUE
+
+(defn vertices [G]
+  (reduce (fn [vs [v edges]]
+            (-> (into vs (keys edges))
+                (conj v)))
+    #{}
+    G))
+
+(defn edges [G]
+  (reduce (fn [M [u edges-u]]
+            (concat M (reduce (fn [es [v w]]
+                                (conj es [[u v] (Integer. w)]))
+                       []
+                       edges-u)))
+    []
+    G))
+
+;;(def dijkstra-simple (load-weighted-graph "dijkstra_simple_test_case.txt"))
+;;TODO: use a heap to store unexplored vertices to speed things up
+(defn shortest-paths
+  ([G s] (shortest-paths G s {s 0}
+                        (reduce (fn [h v]
+                                  (assoc h v infinity))
+                                {}
+                                (disj (vertices G) s))
+                        (set (edges G))
+    ))
+  ([G s found searching edges]
+     (loop [found found, searching searching, edges edges]
+       (if (empty? searching)
+       found
+       (let [V (set (keys found))
+             X (set (keys searching))
+             possible-edges
+             (filter
+              (fn [[[u v] w]]
+                (and (V u) (X v)))
+              edges)
+             next-edge
+             (reduce (fn [[_ min-score :as min-edge] [[u v :as e] w]]
+                       (let [greedy-score (+ (found u) w)]
+                         (if (< greedy-score min-score)
+                         [e greedy-score]
+                         min-edge)))
+                     [[nil nil] infinity]
+                     possible-edges)
+             [[u v] greedy-score] next-edge]
+         (recur (assoc found v greedy-score)
+                (dissoc searching v)
+                (disj edges next-edge))
+         ))))
+  )
+
+(defn distances-to [G s nodes]
+  (map (shortest-paths G s) nodes))
+
+(comment (defn homework-assignment []
+   (clojure.string/join "," (distances-to bigger-graph "1" (map str [7,37,59,82,99,115,133,165,188,197])))))
---
+++ test/algorithms/dijkstra_shortest_path_test.clj
@@ -0,0 +1,17 @@
+(ns algorithms.dijkstra-shortest-path-test
+  (:use clojure.test
+        [algorithms.core :only [load-weighted-graph]]
+        algorithms.dijkstra-shortest-path))
+
+
+(deftest test-shortest-path-in-simple-graph
+  (testing "Find all shortest path from source in very simple graph"
+    (let [G (load-weighted-graph "dijkstra_simple_test_case.txt")]
+      (is (= (shortest-paths G "1")
+             {"1" 0, "2" 1, "3" 3, "4" 6})))))
+
+(deftest test-shortest-path-in-simple-graph
+  (testing "Find all shortest path from source in very simple graph"
+    (let [G (load-weighted-graph "dijkstra_test_case_4.txt")]
+      (is (= (shortest-paths G "1")
+            {"1" 0, "2" 7, "3" 9, "4" 20, "5" 20, "6" 11})))))
